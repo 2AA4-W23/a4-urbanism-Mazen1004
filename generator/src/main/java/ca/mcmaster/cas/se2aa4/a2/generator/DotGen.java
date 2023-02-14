cda6cf9e504fbe62448c5a9924c6d5b8a44d414e
kelvin-u <yukaiwenn@gmail.com>
2023-02-14 00:26:55 -0500

    spelling

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -22,7 +22,7 @@ public class DotGen {
     private final int square_size = 20;
 
     public Mesh generate() {
-        int numOfVerticies = 0;
+        int numOfVertices = 0;
 
         ArrayList<Vertex> vertices = new ArrayList<>();
         // Create all the vertices
@@ -38,16 +38,16 @@ public class DotGen {
                 vertices.add(Vertex.newBuilder().setX((double) x + square_size).setY((double) y + square_size).build());
                 // (0,0) (20,0) (0,20) (20,20)
                 //   0      1     2       3
-                numOfVerticies+=4;
+                numOfVertices+=4;
 
             }
         }
         ArrayList<Segment> segments = new ArrayList<>();
 
-        for (int j=0; j <= numOfVerticies - 2; j+=2){
+        for (int j=0; j <= numOfVertices - 2; j+=2){
                 segments.add(Segment.newBuilder().setV1Idx(j).setV2Idx(j + 1).build());
         }
-        for (int i=0; i <= numOfVerticies - 3; i+=2) {
+        for (int i=0; i <= numOfVertices - 3; i+=2) {
             // Checks to see if the next top left value in the square segment is in a new square column.
             // Eliminate lines being drawn diagonally from the last value in the column to the first value in the next column.
             if ((i + 2) % 100 != 0) {
